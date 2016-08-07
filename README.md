c835d7f6951f94b24fd23ed0ff67aa9e9a29352b
Chai Phonbopit <phonbopit@gmail.com>
2016-08-07 11:41:58 +0700

    add hash and update README

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 basic file hashing
 
 
-## Mac OS
+Run native tool on OSX
 
 ```
 shasum -a 256 test_file.txt
 ```
 
+Run `app.js` for built in crypto module in nodejs
+
+```
+node app.js
+```
+
+## Reference
+
+- [Crypto: Node.js v6.3.1 Documentation](https://nodejs.org/api/crypto.html)
--- app.js
+++ app.js
@@ -2,17 +2,25 @@
 
 const crypto = require('crypto');
 const hmac = crypto.createHmac('sha256', 'secretJingJing');
+const hash = crypto.createHash('sha256');
 
-hmac.update('Unit tests are like condoms. Nobody uses them. LOL');
-const hash = hmac.digest('hex');
-console.log('hex : ', hash);
+const QUOTE_OF_THE_DAY = 'Unit tests are like condoms. Nobody uses them. LOL';
+// HMAC
+hmac.update(QUOTE_OF_THE_DAY);
+const result = hmac.digest('hex');
+console.log('hex : ', result);
 
 // **Note: hmac.digest() can not be used again after called.
 
 
 // checksum
-if ('d6530c7b10ac7216f13867fbb2764eb8b21efbbbd5cc93ca5a13994565dc4aa0' === hash) {
+if ('d6530c7b10ac7216f13867fbb2764eb8b21efbbbd5cc93ca5a13994565dc4aa0' === result) {
   console.log('hoolay! awesome quote');
 } else {
   console.log('nobody trusts you');
 }
+
+
+// HASH
+hash.update(QUOTE_OF_THE_DAY);
+console.log('Hash : ', hash.digest('hex'));
