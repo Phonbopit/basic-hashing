4d24b512234023f6755b21f461f5b775074974f4
Chai Phonbopit <phonbopit@gmail.com>
2016-08-07 11:36:55 +0700

    add checksum

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const crypto = require('crypto');
 const hmac = crypto.createHmac('sha256', 'secretJingJing');
 
 hmac.update('Unit tests are like condoms. Nobody uses them. LOL');
-console.log('base64 : ', hmac.digest('base64'));
-// console.log('hex : ', hmac.digest('hex'));
+const hash = hmac.digest('hex');
+console.log('hex : ', hash);
 
 // **Note: hmac.digest() can not be used again after called.
+
+
+// checksum
+if ('d6530c7b10ac7216f13867fbb2764eb8b21efbbbd5cc93ca5a13994565dc4aa0' === hash) {
+  console.log('hoolay! awesome quote');
+} else {
+  console.log('nobody trusts you');
+}
